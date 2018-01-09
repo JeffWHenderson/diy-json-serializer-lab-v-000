3f8027964ca5631aba3d0068108e2869c40b3516
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-09 19:03:09 +0000

    automatically backed up by learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,10 +1,10 @@
 class ProductSerializer
-  name, price, inventory and description
 
   def self.serialize(product)
       serialized_product = '{'
       serialized_product += '"id": ' + product.id.to_s + ', '
       serialized_product += '"name": "' + product.name + '", '
+      serialized_product += '"inventory": "' + product.inventory + '", '
       serialized_product += '"price": "' + product.price + '", '
       serialized_product += '"description": "' + product.description + '"}'
   end
