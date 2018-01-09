931b9c30d58bc304c7190cc976b2f3cdc65a1832
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-09 19:10:08 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,7 +24,7 @@ class ProductsController < ApplicationController
 
   def data
     @product = Product.find(params["id"])
-    render json: "#{ProductSerializer.serializ(@product)}"
+    render json: "#{ProductSerializer.serialize(@product)}"
   end
 
   private
